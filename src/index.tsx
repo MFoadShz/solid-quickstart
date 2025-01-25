145f82b1a0be01c6a388a24f82c679922d73b69a
MFoadShz <123937641+MFoadShz@users.noreply.github.com>
2025-01-25 07:21:27 +0000

    Updated Home page and added customer form

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,9 @@
-/* @refresh reload */
-import './index.css';
+import { render } from "solid-js/web";
+import { Router } from "@solidjs/router";
+import App from "./app";
 
-import { render, Suspense } from 'solid-js/web';
-
-import App from './app';
-import { Router } from '@solidjs/router';
-import { routes } from './routes';
-
-const root = document.getElementById('root');
-
-if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-  throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
-  );
-}
-
-render(
-  () => <Router root={(props) => <App>{props.children}</App>}>{routes}</Router>,
-  root,
-);
+render(() => (
+  <Router>
+    <App />
+  </Router>
+), document.getElementById("root"));
