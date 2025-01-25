7699faf1edaa05aa2fc38488788f40683b1b04a3
MFoadShz <123937641+MFoadShz@users.noreply.github.com>
2025-01-25 07:29:21 +0000

    Updated Home page and added customer form

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,14 @@
-import { Routes, Route } from "@solidjs/router";
+import { Route } from "@solidjs/router";
 import CustomerForm from "./pages/CustomerForm";
-import ServiceSelection from "./pages/ServiceSelection";
-import InvoicePreview from "./pages/InvoicePreview";
+// import ServiceSelection from "./pages/ServiceSelection";
+// import InvoicePreview from "./pages/InvoicePreview";
 
 export default function App() {
   return (
     <div dir="rtl" class="font-sans">
-      <Routes>
         <Route path="/" component={CustomerForm} />
-        <Route path="/services" component={ServiceSelection} />
-        <Route path="/invoice" component={InvoicePreview} />
-      </Routes>
+        {/* <Route path="/services" component={ServiceSelection} />
+        <Route path="/invoice" component={InvoicePreview} /> */}
     </div>
   );
 }
